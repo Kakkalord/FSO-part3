a5aeb8c1420f9f2d455a3dd3f4aa037e375d881a
Kakkalord <clarenceywang@gmail.com>
2024-08-05 12:43:33 +0800

    for part 3.10

---
+++ index.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+const info = (persons) => {
+  const now = new Date()
+  const currentDateTime = now.toLocaleString()
+
+  return (
+    `Phonebook has info for ${persons.length} people
+    <p>${currentDateTime}<p>`
+  )
+}
+
+const generateRandomId = () => {
+  return (
+    Math.floor(Math.random() * 1000)
+  )
+}
+
+app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+    console.log('Method', request.method)
+    console.log('Path: ', request.path)
+    console.log('Body: ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(requestLogger)
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello Clarence!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  response.send(info(persons))
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  notes = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons/:id', (request,response) => {
+  const body = request.body
+  
+  if (!body.content || !body.name || !body.number) {
+    return (response.status(400).json({
+      error: 'content missing'
+    }))
+  } else if (persons.some(person => person.name === body.name)) {
+    return (response.status(403).json({
+      error: 'person exists already'
+    }))
+  }
+
+  const person = {
+    id: generateRandomId,
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+
+  response.json(note)
+})
